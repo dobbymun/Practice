7e70ea6402cf7a39ea5bc864c0b1248acc5e24df
dobbymun <tearofcake@gmail.com>
2023-05-10 19:38:55 +0900

    feat: 배열 회전시키기 [lv0-day10]

---
+++ progammers/src/main/java/com/example/progammers/day/day10/TurnArray.java
@@ -0,0 +1,41 @@
+package com.example.progammers.day.day10;
+
+/** 배열 회전시키기
+ * 정수가 담긴 배열 numbers와 문자열 direction가 매개변수로 주어집니다.
+ * 배열 numbers의 원소를 direction방향으로 한 칸씩 회전시킨 배열을 return하도록 solution 함수를 완성해주세요.
+ *
+ * 제한사항
+ * 3 ≤ numbers의 길이 ≤ 20
+ * direction은 "left" 와 "right" 둘 중 하나입니다.
+ *
+ * 예시
+ * numbers 가 [1, 2, 3]이고 direction이 "right" 이므로 오른쪽으로 한 칸씩 회전시킨 [3, 1, 2]를 return합니다.
+ * numbers 가 [4, 455, 6, 4, -1, 45, 6]이고 direction이 "left" 이므로 왼쪽으로 한 칸씩 회전시킨 [455, 6, 4, -1, 45, 6, 4]를 return합니다.
+ */
+
+public class TurnArray {
+    public int[] solution(int[] numbers, String direction) {
+        int[] answer = new int[numbers.length];
+
+        if(direction.equals("left")){
+            for(int i=0; i< numbers.length; i++){
+                if(i == numbers.length-1){
+                    answer[i]= numbers[0];
+                }else{
+                    answer[i]= numbers[i+1];
+                }
+            }
+
+        }else {
+            for(int i=0; i< numbers.length; i++){
+                if(i == 0){
+                    answer[i]= numbers[numbers.length-1];
+                }else{
+                    answer[i]= numbers[i-1];
+                }
+            }
+
+        }
+        return answer;
+    }
+}
